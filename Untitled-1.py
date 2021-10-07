a92c2d6a1361322d05129d398d9f35a35bf8bbd7
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2021-10-07 12:57:54 +0200

    zadani1

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+def foo(a,b):
+    print(a+'. '+b)
+foo('S','Celoch')
+def foo2(i,n):
+    print(i[0].capitalize()+'. '+n.capitalize())
+foo2('sebastian','celoch')
+def foo3(r,r2,w):
+    r = str(r)
+    r2 = str(r2)
+    r+=r2
+    x=int(r)-w
+    print(x)
+foo3(20,21,40)
+def foo5(a,b):
+    if(((a>0 and b>0)and b!=0)):
+        print(a/b)
+foo5(4,2)
