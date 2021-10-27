f3596f5d8b8b20cc6c7f2646a9fc67468aef1dea
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2021-10-27 17:12:01 +0200

    projektpoparwiony

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-    #dodajemy element na poczatek listy  
+    #inicjujemy glowe w liscie
       
     def push(self,new_data: Any) -> None:
         new_node = Node(new_data) #nowy node przyjmuje wartosc new_data
@@ -47,7 +47,7 @@ class LinkedList:
             cur = cur.next
         return count
 
-    def deleteNode(self, key: Node) ->Any:
+    def remove(self, key: Node) ->Any:
         temp = self.head
         if(temp is not None):
             if(temp.data == key):
@@ -67,21 +67,21 @@ class LinkedList:
     def pop(self) ->Any:
         node = self.head
         self.head = node.next
-        return node
+        return 'usuniety element to = '+str(node.data)
 
     def remove_last(self) ->Any:
         node = self.head
-        for x in range(self.length() - 2):
+        for x in range(self.length()-2):
             node = node.next
         node2 = node.next
         node.next = None
-        return node2
+        return 'usuniety element to = '+str(node2.data)
 
 # print listy
     def printList(self):
         temp = self.head 
         while(temp):
-            print (temp.data, ' -> ',end=" ")
+            print (temp.data,end=" -> ")
             temp = temp.next
 
 class Stack:
@@ -108,41 +108,41 @@ class Stack:
         pop_node = self.head
         self.head = self.head.next
         pop_node.next = None
-        return pop_node.data
+        return 'usuniety element to = '+str(pop_node.data)
 
     def print(self):
-        iternode = self.head
-        while(iternode != None):
-            print(iternode.data,"->",end = " ")
-            iternode = iternode.next
+        temp = self.head
+        while(temp != None):
+            print(temp.data,end = " -> ")
+            temp = temp.next
         return
 class Queue:
     def __init__(self):
-        self.head = self.rear = None
+        self.head = self.back = None
 
-    def enqueue(self,element) -> None:
+    def enqueue(self,element: Any) -> None:
         temp = Node(element)
-        if self.rear == None:
-            self.front = self.rear = temp
+        if self.back == None:
+            self.front = self.back = temp
             return
-        self.rear.next = temp
-        self.rear = temp
+        self.back.next = temp
+        self.back = temp
 
     def dequeue(self) -> None:
         temp = self.front
         self.front = temp.next
         if(self.front == None):
-            self.rear = None
+            self.back = None
 
     def peek(self) -> Any:
         if self.front:
             return self.front.data
 
     def print(self):
-        x = self.front
-        while x:
-            print(x.data, end=' <- ')
-            x = x.next
+        temp = self.front
+        while temp:
+            print(temp.data, end=' <- ')
+            temp = temp.next
         print()
     
     def len(self):
@@ -161,22 +161,24 @@ llist.append(5)
 llist.push(1)
 llist.push(5)
 llist.push(3)
-llist.deleteNode(6)
+llist.remove(1)
 llist.insert(llist.head.next,5)
-llist.pop()
-llist.remove_last()
+print(llist.pop())
+print(llist.remove_last())
 print('dlugosc listy = ',llist.length())
 print(llist.node(0))
 llist.printList()
 print('\n')
+
 Stackk = Stack()
 Stackk.push(10)
 Stackk.push(5)
 Stackk.push(33)
-Stackk.pop()
+print(Stackk.pop())
 print(Stackk.len())
 Stackk.print()
 print('\n')
+
 q = Queue()
 q.enqueue(10)
 q.enqueue(20)
